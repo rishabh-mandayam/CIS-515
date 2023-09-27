6cda537309d5561240c972566482b5986dcaa5fe
rishabh.mandayam@gmail.com <141846398+rishabh-mandayam@users.noreply.github.com>
2023-09-27 19:29:22 -0400

    changed capital M to lower case m

--- student-code1a/makelist.m
+++ student-code1a/makelist.m
@@ -1,5 +1,5 @@
 function lnodes = makelist(lpoly)
-    m= size(lpoly, 3); % num of polygons 
+    m = size(lpoly, 3); % num of polygons 
     num_control_points = size(lpoly, 2); % num of control points
     total_points = (m * (num_control_points - 1))+ 1; 
     
@@ -9,7 +9,7 @@ function lnodes = makelist(lpoly)
     lnodes(:, 1:num_control_points) = lpoly(:, :, 1);
     
     % Concat the rest of the cpolys, skipping the first point
-    for i = 2:M
+    for i = 2:m
         start = (i - 1) * (num_control_points - 1) + 1;
         finish = start + num_control_points - 2;
         lnodes(:, start:finish) = lpoly(:, 2:end, i);
