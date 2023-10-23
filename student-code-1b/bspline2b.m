bd88011ae81c84f83e074114f691bdb69294709b
rishabh.mandayam@gmail.com <141846398+rishabh-mandayam@users.noreply.github.com>
2023-10-23 18:26:17 -0400

    Update bspline2b.m
    Finished Part 1

--- student-code-1b/bspline2b.m
+++ student-code-1b/bspline2b.m
@@ -17,6 +17,70 @@ function [Bx, By] = bspline2b(dx,dy,N,nn,drawb)
 % Works if N >= 4.
 
  %%% COMPUTE Bx AND By HERE %%%
+    Bx = zeros(N-2,4);
+    By = zeros(N-2,4);
+
+    for i=1:(N-2)
+        if i == 1
+            Bx(i, 1) = dx(1);
+            Bx(i, 2) = dx(2);
+            Bx(i, 3) = 1/2*dx(2) + 1/2*dx(3);
+            Bx(i, 4) = 1/4*dx(2) + 7/12*dx(3) +1/6*dx(4);
+
+            
+            By(i, 1) = dy(1);
+            By(i, 2) = dy(2);
+            By(i, 3) = 1/2*dy(2) + 1/2*dy(3);
+            By(i, 4) = 1/4*dy(2) + 7/12*dy(3) +1/6*dy(4);
+        
+        elseif i == 2
+            Bx(i, 1) = Bx(i - 1, 4);
+            Bx(i, 2) = 2/3*dx(3) + 1/3*dx(4);
+            Bx(i, 3) = 1/3*dx(3) + 2/3*dx(4);
+            Bx(i, 4) = 1/6*dx(3) + 4/6*dx(4) + 1/6*dx(5);
+   
+            
+            By(i, 1) = By(i - 1, 4);
+            By(i, 2) = 2/3*dy(3) + 1/3*dy(4);
+            By(i, 3) = 1/3*dy(3) + 2/3*dy(4);
+            By(i, 4) = 1/6*dy(3) + 4/6*dy(4) + 1/6*dy(5);
+
+        elseif i == N - 3
+            Bx(i, 1) = Bx(i - 1, 4);
+            Bx(i, 2) = 2/3*dx(N-2) + 1/3*dx(N-1);
+            Bx(i, 3) = 1/3*dx(N-2) + 2/3*dx(N-1);
+            Bx(i, 4) = 1/6*dx(N-2) + 7/12*dx(N-1) + 1/4*dx(N);
+
+
+            By(i, 1) = By(i - 1, 4);
+            By(i, 2) = 2/3*dy(N-2) + 1/3*dy(N-1);
+            By(i, 3) = 1/3*dy(N-2) + 2/3*dy(N-1);
+            By(i, 4) = 1/6*dy(N-2) + 7/12*dy(N-1) + 1/4*dy(N);
+        elseif i == N - 2
+            Bx(i, 1) = Bx(i-1, 4);
+            Bx(i, 2) = 1/2*dx(N-1) + 1/2*dx(N);
+            Bx(i, 3) = dx(N);
+            Bx(i, 4) = dx(N + 1);
+
+            
+            By(i, 1) = By(i-1, 4);
+            By(i, 2) = 1/2*dy(N-1) + 1/2*dy(N);
+            By(i, 3) = dy(N);
+            By(i, 4) = dy(N + 1);
+        else
+            Bx(i, 1) = Bx(i - 1, 4);
+            Bx(i, 2) = 2/3*dx(i+1) + 1/3*dx(i+2);
+            Bx(i, 3) = 1/3*dx(i+1) + 2/3*dx(i+2);
+            Bx(i, 4) = 1/6*dx(i+1) + 4/6*dx(i+2) + 1/6*dx(i+3);
+
+
+            By(i, 1) = By(i - 1, 4);
+            By(i, 2) = 2/3*dy(i+1) + 1/3*dy(i+2);
+            By(i, 3) = 1/3*dy(i+1) + 2/3*dy(i+2);
+            By(i, 4) = 1/6*dy(i+1) + 4/6*dy(i+2) + 1/6*dy(i+3);
+        end
+    end
+
 
 % nn is the subdivision level
 % fprintf('numpt = %d \n', numpt)
