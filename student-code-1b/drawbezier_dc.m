e7234ff8ef9cf7ce560eaffb8508b6f9e4ffc298
Alissa Cielecki <55100274+acielecki@users.noreply.github.com>
2023-10-23 20:58:04 -0400

    Update drawbezier_dc.m

--- student-code-1b/drawbezier_dc.m
+++ student-code-1b/drawbezier_dc.m
@@ -6,16 +6,21 @@
 %
 function drawbezier_dc(B,nn,drawb)
  % nn is the subdivision level
+    [x,y] = show_decas_subdiv2(B,nn);
+    
+    random_color = [rand, rand, rand];
 
- %%% DRAW CURVE HERE %%%
- cla holdon
+    % Plot the curve segment as a random color
+    plot(x, y, 'o-', 'Color', random_color, 'LineWidth', 1.5);
+    hold on;
 
- % Plot the curve segment as a random color
- plot(B(1,:),B(2,:) -s)
- if drawb == 1 
-    %%% Plot the Bezier points and segments  as red + %%%
- else
-    %%% Plot the Bezier points as red + %%%
- end
+    % Check the drawb parameter to decide how to plot control points/polygons
+    if drawb == 1
+         %%% Plot the Bezier points and segments  as red + %%%
+        plot(B(1,:), B(2,:), 'r-', 'LineWidth', 1.5);
+        plot(B(1,:), B(2,:), 'r+');
+    else
+        %%% Plot the Bezier points as red + %%%
+        plot(B(1,:), B(2,:), 'r+');
+    end
 end
-
